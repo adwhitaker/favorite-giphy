ebf3993d797d94098d08169920cb967fbbff1e3e
Alex Whitaker <whitaker.alexander@gmail.com>
2016-10-23 15:30:09 -0500

    get and post gif

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var gifs = require('./routes/favorites');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use('/home', gifs);
+
+// app.use('/favorites', gifs);
+
+app.use('/home', function (req, res) {
+  res.send('Hola');
+});
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
