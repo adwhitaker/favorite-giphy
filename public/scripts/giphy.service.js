c068ce7f34747d6c5180f18040fcd24d3fa855dd
Alex Whitaker <whitaker.alexander@gmail.com>
2016-10-23 14:22:33 -0500

    basic functionality search and random gifs

---
+++ public/scripts/giphy.service.js
@@ -0,0 +1,30 @@
+angular.module('favoriteGiphy')
+       .service('giphy', GiphyAPIService);
+
+function GiphyAPIService($http) {
+  var API = 'http://api.giphy.com/v1/gifs/';
+  var KEY = 'dc6zaTOxFJmzC';
+
+  this.searchItem = function (query) {
+    return $http.get(API + 'search', {
+      params: {
+        api_key: KEY,
+        q: query
+      },
+    }).then(function (response) {
+        return response.data.data;
+      });
+  };
+
+  this.randomSearch = function () {
+    return $http.get(API + 'random', {
+      params: {
+        api_key: KEY,
+      },
+    })
+                .then(function (response) {
+                  console.log(response.data.data);
+                  return response.data.data;
+                });
+  };
+};
--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -1,7 +1,26 @@
 angular.module('favoriteGiphy')
-       .controller('HomeController', HomeController);
+.controller('HomeController', HomeController);
 
 function HomeController(giphy) {
   var home = this;
   console.log('HomeController loaded');
-}
+
+  home.pic = '';
+  home.gifs = [];
+
+  home.searchItem = function (query) {
+    giphy.searchItem(encodeURI(query))
+         .then(function (gifs) {
+            home.gifs = gifs;
+          });
+  };
+
+  home.randomSearch = function () {
+    home.gifs = [];
+    giphy.randomSearch()
+         .then(function (gif) {
+            home.pic = gif.image_url;
+          });
+  };
+
+};
